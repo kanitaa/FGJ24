65fb0a2017a9151a6a933693eda0bd6d42bcd6e9
AhtiAhde <ahti.ahde@gmail.com>
2024-01-27 19:48:51 +0200

    Fix merge conflicts and add Dad Jokes

--- Assets/Scripts/AlienManager.cs
+++ Assets/Scripts/AlienManager.cs
@@ -1,8 +1,23 @@
+using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+
+public class JokeList
+{
+    public List<string> jokes;
+    public List<string> punchlines;
+}
+
+public class Joke
+{
+    public string joke;
+    public string punchline;
+}
 
 public class AlienManager : MonoBehaviour
 {
@@ -10,35 +25,49 @@ public class AlienManager : MonoBehaviour
     [SerializeField] Button _aButton, _bButton;
     [SerializeField] List<DadJoke> _dadJokes;
 
-    int _jokeIndex = 0;
+    private List<Joke> jokeList;
 
     private void Start()
     {
+        using (StreamReader r = new StreamReader("Assets/dad-jokes.json"))
+        {
+            string json = r.ReadToEnd();
+            jokeList = JsonConvert.DeserializeObject<List<Joke>>(json);
+        }
+    
         GenerateJoke();
     }
+
     void GenerateJoke()
     {
         _aButton.onClick.RemoveAllListeners();
         _bButton.onClick.RemoveAllListeners();
 
-        _alienText.text = _dadJokes[_jokeIndex].punchLine;
+        Debug.Log(jokeList.Count);
+        int correct = Random.Range(0, jokeList.Count);
+        int incorrect = Random.Range(0, jokeList.Count);
 
-        _answerA.text = _dadJokes[_jokeIndex].option[0];
-        _answerB.text = _dadJokes[_jokeIndex].option[1];
+        _alienText.text = jokeList[correct].punchline;
 
-        if (_dadJokes[_jokeIndex].answerId == 0)
+        string correct_joke = jokeList[correct].joke;
+        string incorrect_joke = jokeList[incorrect].joke;
+
+        
+
+        if (Random.Range(0,2) == 0)
         {
+            _answerA.text = correct_joke;
+            _answerB.text = incorrect_joke;
             _aButton.onClick.AddListener(CorrectAnswer);
             _bButton.onClick.AddListener(WrongAnswer);
         }
-        if (_dadJokes[_jokeIndex].answerId == 1)
+        else 
         {
+            _answerA.text = incorrect_joke;
+            _answerB.text = correct_joke;
             _aButton.onClick.AddListener(WrongAnswer);
             _bButton.onClick.AddListener(CorrectAnswer);
         }
-
-        if (_jokeIndex < _dadJokes.Count - 1) _jokeIndex++;
-        else _jokeIndex = 0;
     }
 
     void CorrectAnswer()
