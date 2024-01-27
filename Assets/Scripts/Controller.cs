ca3569617e83ce70f12b5d04661e3ac70ba0db3f
kanitaa <janita.kor@gmail.com>
2024-01-27 16:43:31 +0200

    game loop changes

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -8,8 +8,7 @@ public class Controller : MonoBehaviour
     public GameObject canvas;
     public GameObject background;
     public GameObject overlay;
-    public GameObject example1;
-    public GameObject example2;
+    public GameObject scene1, scene2, scene3, scene4, scene7, scene8, scene9, alienPopup;
     public GameObject form;
     private int state = 0;
     private GameObject activeOverlay;
@@ -17,49 +16,95 @@ public class Controller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Next();   
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void Next()
     {
         if (this.state == 0) {
-            this.activeOverlay = Instantiate(example1, overlay.transform.position, Quaternion.identity, overlay.transform);
-            this.activeOverlay.transform.SetParent(overlay.transform);
-            this.activeOverlay.transform.localScale += new Vector3(1960, 1080, 1);
+            //this.activeOverlay = Instantiate(scene1, overlay.transform.position, Quaternion.identity, overlay.transform);
+            //this.activeOverlay.transform.SetParent(overlay.transform);
+            //this.activeOverlay.transform.localScale += new Vector3(1960, 1080, 1);
             // Push example 1 to canvas
+            scene1.SetActive(true);
             this.state = 1;
         }
         else if (this.state == 1) {
-            Destroy(this.activeOverlay);
-            this.activeOverlay = Instantiate(example2, overlay.transform.position, Quaternion.identity, overlay.transform);
-            this.activeOverlay.transform.SetParent(overlay.transform);
-            this.activeOverlay.transform.localScale += new Vector3(1, 1, 1);
+            //Destroy(this.activeOverlay);
+            //this.activeOverlay = Instantiate(scene2, overlay.transform.position, Quaternion.identity, overlay.transform);
+            //this.activeOverlay.transform.SetParent(overlay.transform);
+            //this.activeOverlay.transform.localScale += new Vector3(1, 1, 1);
             // Push example 2 to canvas
+            scene1.SetActive(false);
+            scene2.SetActive(true);
             this.state = 2;
         }
-        else if (this.state == 2) {
-            Destroy(this.activeOverlay);
+        else if (this.state == 2)
+        {
+            //Destroy(this.activeOverlay);
+            //this.activeOverlay = Instantiate(scene3, overlay.transform.position, Quaternion.identity, overlay.transform);
+            //this.activeOverlay.transform.SetParent(overlay.transform);
+            //this.activeOverlay.transform.localScale += new Vector3(1, 1, 1);
+            // Push example 2 to canvas
+            scene2.SetActive(false);
+            scene3.SetActive(true);
+            this.state = 3;
+        }
+        else if (this.state == 3)
+        {
+            //Destroy(this.activeOverlay);
+            //this.activeOverlay = Instantiate(scene4, overlay.transform.position, Quaternion.identity, overlay.transform);
+            //this.activeOverlay.transform.SetParent(overlay.transform);
+            //this.activeOverlay.transform.localScale += new Vector3(1, 1, 1);
+            // Push example 2 to canvas
+            scene3.SetActive(false);
+            scene4.SetActive(true);
+            this.state = 4;
+        }
+        else if (this.state == 4) {
+            //Destroy(this.activeOverlay);
             form.SetActive(true);
+            scene4.SetActive(false);
             GameObject nextButton = overlay.transform.GetChild(0).gameObject;
             nextButton.SetActive(false);
             //this.activeOverlay = Instantiate(form, overlay.transform.position, Quaternion.identity, overlay.transform);
             //this.activeOverlay.transform.SetParent(overlay.transform);
             //this.activeOverlay.transform.localScale += new Vector3(1, 1, 1);
             // Push example 2 to canvas
-            this.state = 3;
+            this.state = 5;
         }
-        else if (this.state == 3)
+        else if (this.state == 5)
+        {
+            //Title screen
+            scene7.SetActive(true);
+            GameObject nextButton = overlay.transform.GetChild(0).gameObject;
+            nextButton.SetActive(true);
+            this.state = 6;
+        }
+        else if (this.state == 6)
         {
             //Surgery room
-            Destroy(this.activeOverlay);
-            
-            this.state = 4;
+            scene7.SetActive(false);
+            scene8.SetActive(true);
+            this.state = 7;
         }
+        else if (this.state == 7)
+        {
+            
+            scene8.SetActive(false);
+            scene9.SetActive(true);
+            this.state = 8;
+        }
+        else if (this.state == 8)
+        {
+            //Alien popup
+            scene9.SetActive(false);
+            alienPopup.SetActive(true);
+            GameObject nextButton = overlay.transform.GetChild(0).gameObject;
+            nextButton.SetActive(false);
+            this.state = 9;
+        }
+      
     }
 }
